34b0093d20130c3577e015e8a74162bd122efe0c
Pi-Gul <photon-drive@protonmail.com>
2020-12-04 20:46:26 +0100

    Add working solution for Day-4-1

--- day-4/day-4-1.js
+++ day-4/day-4-1.js
@@ -6,18 +6,41 @@ const inputFile = fs.readFileSync('./day-4-input.txt').toString().split('\r\n');
 const passportArr = []
 let newPassport = {};
 inputFile.forEach((line) => {
-	strArr = line.split(' ');
-	strArr.forEach((str) => {
-		if(str === '') {
-			passportArr.push(newPassport);
-			newPassport = {};
-		};
-		let key = str.split(':')[0];
-		let value = str.split(':')[1];
-		newPassport[key] = value;
-	});
+	if(line === '') {
+		passportArr.push(newPassport);
+		newPassport = {};
+	} else {
+		strArr = line.split(' ');
+		strArr.forEach((str) => {
+			let key = str.split(':')[0];
+			let value = str.split(':')[1];
+			newPassport[key] = value;
+		});
+	};
 });
+passportArr.push(newPassport);
 
+//console.log(passportArr);
+
+const checkPassportValidity = (passportArr) => {
+	return passportArr.filter((passport) => {
+		let keys = Object.keys(passport);
+		//console.log(keys);
+		//console.log(keys.includes('byr'));
+		let conditions = keys.includes('byr') 
+		&& keys.includes('iyr') 
+		&& keys.includes('eyr') 
+		&& keys.includes('hgt') 
+		&& keys.includes('hcl') 
+		&& keys.includes('ecl') 
+		&& keys.includes('pid')
+		&& (keys.includes('cid') || !keys.includes('cid'));
+		//console.log(conditions);
+		return conditions;
+	});
+};
 
 //console.log(inputFile);
-console.log(passportArr);
+//console.log(passportArr);
+console.log(checkPassportValidity(passportArr).length);
+//console.log(checkPassportValidity(passportArr)[10]);
